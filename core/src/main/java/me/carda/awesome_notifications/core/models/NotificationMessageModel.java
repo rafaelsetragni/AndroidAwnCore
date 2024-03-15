bbd0a69cebf4262b61be7f7b894bf06ab35a2a55
rafaelsetragni <rafaelsetra@gmail.com>
2024-03-15 07:42:02 -0300

    Update Notification Builder and Native Dependencies
    - Modified the notification builder logic to no longer require setting a summary for the Messaging layout. This change addresses and prevents the exception "User must be valid and have a name" by allowing notifications to be constructed without this previously mandatory field.
    - Updated all native dependencies to their latest versions, ensuring compatibility and taking advantage of recent bug fixes and performance improvements.

--- core/src/main/java/me/carda/awesome_notifications/core/models/NotificationMessageModel.java
+++ core/src/main/java/me/carda/awesome_notifications/core/models/NotificationMessageModel.java
@@ -11,15 +11,17 @@ import me.carda.awesome_notifications.core.exceptions.AwesomeNotificationsExcept
 public class NotificationMessageModel extends AbstractModel {
 
     public String title;
+    public String summary;
     public String message;
     public String largeIcon;
     public Long timestamp;
 
     public NotificationMessageModel(){}
 
-    public NotificationMessageModel(String title, String message, String largeIcon){
+    public NotificationMessageModel(String title, String summary, String message, String largeIcon){
 
         this.title = title;
+        this.summary = summary;
         this.message = message;
         this.largeIcon = largeIcon;
         this.timestamp = System.currentTimeMillis();
@@ -29,6 +31,7 @@ public class NotificationMessageModel extends AbstractModel {
     public NotificationMessageModel fromMap(Map<String, Object> arguments) {
 
         title     = getValueOrDefault(arguments, Definitions.NOTIFICATION_TITLE, String.class, null);
+        summary   = getValueOrDefault(arguments, Definitions.NOTIFICATION_SUMMARY, String.class, null);
         message   = getValueOrDefault(arguments, Definitions.NOTIFICATION_MESSAGES, String.class, null);
         largeIcon = getValueOrDefault(arguments, Definitions.NOTIFICATION_LARGE_ICON, String.class, null);
         timestamp = getValueOrDefault(arguments, Definitions.NOTIFICATION_TIMESTAMP, Long.class, null);
@@ -41,6 +44,7 @@ public class NotificationMessageModel extends AbstractModel {
         Map<String, Object> dataMap = new HashMap<>();
 
         putDataOnSerializedMap(Definitions.NOTIFICATION_TITLE, dataMap, title);
+        putDataOnSerializedMap(Definitions.NOTIFICATION_SUMMARY, dataMap, summary);
         putDataOnSerializedMap(Definitions.NOTIFICATION_MESSAGES, dataMap, message);
         putDataOnSerializedMap(Definitions.NOTIFICATION_LARGE_ICON, dataMap, largeIcon);
         putDataOnSerializedMap(Definitions.NOTIFICATION_TIMESTAMP, dataMap, timestamp);
